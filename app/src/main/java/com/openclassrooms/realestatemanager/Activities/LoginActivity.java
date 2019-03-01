33675df0599a5f64a151506c0a72d0039b3a4a52
Benjamin Corben <corbenbenjamin@gmail.com>
2019-03-01 15:42:24 +0100

    Forgot your password designed but email needs to be sent

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/LoginActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/LoginActivity.java
@@ -3,10 +3,12 @@ package com.openclassrooms.realestatemanager.Activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,6 +33,10 @@ import java.util.Map;
 public class LoginActivity extends AppCompatActivity {
 
     Boolean newAccount = false;
+    Boolean emailExists = false;
+    String password;
+    Boolean forgotYourPasswordBool = false;
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -41,35 +47,68 @@ public class LoginActivity extends AppCompatActivity {
         SharedPreferences mPrefs = getSharedPreferences("SHARED", MODE_PRIVATE);
 
 
-        if (mPrefs.getString("username", null) != null){
+        if (mPrefs.getString("username", null) != null) {
             Intent myIntent = new Intent(this, MainActivity.class);
             Toast.makeText(this, "Welcome back, " + mPrefs.getString("username", null), Toast.LENGTH_LONG).show();
             startActivity(myIntent);
         }
 
         EditText usernameET = findViewById(R.id.usernameET);
+        TextView usernameTV = findViewById(R.id.usernameTV);
         EditText passwordET = findViewById(R.id.passwordET);
+        TextView passwordTV = findViewById(R.id.passwordTV);
         EditText emailET = findViewById(R.id.emailET);
         TextView emailTV = findViewById(R.id.emailTV);
         Button createButton = findViewById(R.id.createAccountButton);
         Button loginButton = findViewById(R.id.loginButton);
         Button validateButton = findViewById(R.id.validateButton);
+        TextView forgotYourPassword = findViewById(R.id.forgotYourPassword);
 
-        emailET.setVisibility(View.INVISIBLE);
-        emailTV.setVisibility(View.INVISIBLE);
+        forgotYourPassword.setOnClickListener(v -> {
+            forgotYourPasswordBool = true;
+            passwordET.setVisibility(View.INVISIBLE);
+            usernameET.setVisibility(View.INVISIBLE);
+            passwordTV.setVisibility(View.INVISIBLE);
+            usernameTV.setVisibility(View.INVISIBLE);
+
+            emailET.setVisibility(View.VISIBLE);
+            emailTV.setVisibility(View.VISIBLE);
+
+
+        });
+
+        emailET.setVisibility(View.VISIBLE);
+        emailTV.setVisibility(View.VISIBLE);
+
+
+        validateButton.setText("Login");
 
         createButton.setBackground(null);
         createButton.setTextColor(Color.parseColor("#696969"));
 
         createButton.setOnClickListener(v -> {
+            forgotYourPasswordBool = false;
+
             loginButton.setBackground(null);
             loginButton.setTextColor(Color.parseColor("#696969"));
+
+            emailET.setText("");
+            passwordET.setText("");
+            usernameET.setText("");
 
             createButton.setBackground(getDrawable(R.color.colorAccent));
             createButton.setTextColor(Color.parseColor("#000000"));
 
             emailET.setVisibility(View.VISIBLE);
             emailTV.setVisibility(View.VISIBLE);
+            passwordTV.setVisibility(View.VISIBLE);
+            passwordET.setVisibility(View.VISIBLE);
+            usernameET.setVisibility(View.VISIBLE);
+            usernameTV.setVisibility(View.VISIBLE);
+
+            forgotYourPassword.setVisibility(View.INVISIBLE);
+
+            validateButton.setText("Create account");
 
             newAccount = true;
         });
@@ -77,37 +116,65 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(v -> {
             createButton.setBackground(null);
             createButton.setTextColor(Color.parseColor("#696969"));
+            forgotYourPasswordBool = false;
+
+            emailET.setText("");
+            passwordET.setText("");
+            usernameET.setText("");
 
             loginButton.setBackground(getDrawable(R.color.colorAccent));
             loginButton.setTextColor(Color.parseColor("#000000"));
 
             emailET.setVisibility(View.INVISIBLE);
             emailTV.setVisibility(View.INVISIBLE);
+            passwordTV.setVisibility(View.VISIBLE);
+            passwordET.setVisibility(View.VISIBLE);
+            usernameET.setVisibility(View.VISIBLE);
+            usernameTV.setVisibility(View.VISIBLE);
+
+            forgotYourPassword.setVisibility(View.VISIBLE);
+
+            validateButton.setText("Login");
 
             newAccount = false;
         });
 
         validateButton.setOnClickListener(v -> {
             if (newAccount) {
-                //Create new field in Firestore database with email usernam and password
-                DocumentReference mDocRef = FirebaseFirestore.getInstance().collection("user").document();
+                CollectionReference notebookRef = FirebaseFirestore.getInstance().collection("user");
+                notebookRef.get().addOnSuccessListener((queryDocumentSnapshots) -> {
 
-                Map<String, String> dataToSave = new HashMap<>();
+                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        if (documentSnapshot.get("email").equals(emailET.getText().toString())) {
+                            emailExists = true;
+                        }
+                    }
+                    if (!emailExists) {
+                        //Create new field in Firestore database with email usernam and password
+                        DocumentReference mDocRef = FirebaseFirestore.getInstance().collection("user").document();
 
-                dataToSave.put("username", usernameET.getText().toString());
-                dataToSave.put("password", passwordET.getText().toString());
-                dataToSave.put("email", emailET.getText().toString());
+                        Map<String, String> dataToSave = new HashMap<>();
 
-                mDocRef.set(dataToSave, SetOptions.merge());
+                        dataToSave.put("username", usernameET.getText().toString());
+                        dataToSave.put("password", passwordET.getText().toString());
+                        dataToSave.put("email", emailET.getText().toString());
 
-                mPrefs.edit().putString("username", usernameET.getText().toString()).apply();
-                mPrefs.edit().putString("email", emailET.getText().toString()).apply();
+                        mDocRef.set(dataToSave, SetOptions.merge());
 
-                Intent myIntent = new Intent(this, MainActivity.class);
-                startActivity(myIntent);
+                        mPrefs.edit().putString("username", usernameET.getText().toString()).apply();
+                        mPrefs.edit().putString("email", emailET.getText().toString()).apply();
 
+                        Intent myIntent = new Intent(this, MainActivity.class);
+                        startActivity(myIntent);
+                    } else {
+                        Toast.makeText(this, "This email has already been used", Toast.LENGTH_SHORT).show();
+                        emailET.setText("");
+                        passwordET.setText("");
+                        usernameET.setText("");
+                    }
+                });
             } else {
-                CollectionReference notebookRef = FirebaseFirestore.getInstance().collection("house");
+                CollectionReference notebookRef = FirebaseFirestore.getInstance().collection("user");
                 notebookRef.get().addOnSuccessListener((queryDocumentSnapshots) -> {
 
                     for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
@@ -115,22 +182,50 @@ public class LoginActivity extends AppCompatActivity {
 
                             if (passwordET.getText().toString().equals(documentSnapshot.get("password"))) {
                                 Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                                mPrefs.edit().putString("username", usernameET.getText().toString()).apply();
+                                mPrefs.edit().putString("email", emailET.getText().toString()).apply();
+
+                                Intent myIntent = new Intent(this, MainActivity.class);
+                                startActivity(myIntent);
                             }
+                        }
+                    }
+                });
+            }
+            if (forgotYourPasswordBool) {
 
-                            mPrefs.edit().putString("username", usernameET.getText().toString()).apply();
-                            mPrefs.edit().putString("email", emailET.getText().toString()).apply();
+                CollectionReference notebookRef = FirebaseFirestore.getInstance().collection("user");
+                notebookRef.get().addOnSuccessListener((queryDocumentSnapshots) -> {
 
-                            Intent myIntent = new Intent(this, MainActivity.class);
-                            startActivity(myIntent);
+                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        if (emailET.getText().toString().equals(documentSnapshot.get("email"))) {
+
+                            password = (String) documentSnapshot.get("password");
                         }
                     }
 
+                    String[] TO = {"corbenbenjamin@gmail.com"};
+                    String[] CC = {emailET.getText().toString()};
+                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                    emailIntent.setData(Uri.parse("mailto:"));
+                    emailIntent.setType("text/plain");
+
+
+                    emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+                    emailIntent.putExtra(Intent.EXTRA_CC, CC);
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Password");
+                    emailIntent.putExtra(Intent.EXTRA_TEXT, "Your password is: " + password);
+
+                    try {
+                        startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                        finish();
+                    } catch (android.content.ActivityNotFoundException ex) {
+                        Toast.makeText(this,
+                                "There is no email client installed.", Toast.LENGTH_SHORT).show();
+                    }
                 });
             }
         });
-
-
     }
-
-
 }
